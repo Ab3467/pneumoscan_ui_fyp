e00cf368ef08d63044a35c6430e5319f9c372fa9
Ab3467 <ahmed.nums345@gmail.com>
2025-12-21 14:08:52 +0500

    Configured routing and layout

--- pneumoscan/src/App.jsx
+++ pneumoscan/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Result from "./pages/Result";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 export default function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
-      PneumScan UI Working 🚀
-    </h1>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
