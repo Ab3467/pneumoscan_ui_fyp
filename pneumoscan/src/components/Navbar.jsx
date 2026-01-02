0a9bed9e8e944753d0ac7bf2d5247ad498ac8a51
junaidriaz20042025-eng <junaidriaz20042025@gmail.com>
2026-01-02 11:57:44 +0500

    fixing issues

--- pneumoscan/src/components/Navbar.jsx
+++ pneumoscan/src/components/Navbar.jsx
@@ -1,15 +1,24 @@
+// Member 1
+// Navbar.jsx: Top navigation bar
+
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+      {/* Logo / Project Name */}
       <h1 className="text-2xl font-bold text-blue-600">
         PneumScan
       </h1>
 
-      <div className="space-x-6">
-        <Link to="/" className="hover:text-blue-600">Home</Link>
-        <Link to="/upload" className="hover:text-blue-600">Upload X-ray</Link>
+      {/* Navigation Links */}
+      <div className="space-x-6 font-medium">
+        <Link to="/" className="hover:text-blue-600">
+          Home
+        </Link>
+        <Link to="/upload" className="hover:text-blue-600">
+          Upload X-ray
+        </Link>
       </div>
     </nav>
   );
