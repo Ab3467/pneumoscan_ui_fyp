df40537647ca48212138603fdfcb61032897ca1c
junaidriaz20042025-eng <junaidriaz20042025@gmail.com>
2026-01-02 11:58:55 +0500

    changings

--- pneumoscan/src/pages/Home.jsx
+++ pneumoscan/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+// Member 2
+// Home.jsx: Landing page
+
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <div className="text-3xl text-green-600">
-      HOME PAGE WORKING
-    </div>
+    <>
+      <Hero />
+
+      {/* About Section */}
+      <section className="py-16 text-center">
+        <h2 className="text-3xl font-bold mb-4">
+          About PneumScan
+        </h2>
+
+        <p className="max-w-3xl mx-auto text-gray-600">
+          PneumScan is an AI-driven system designed to detect pneumonia
+          from chest X-ray images using deep learning models.
+        </p>
+      </section>
+    </>
   );
 }
